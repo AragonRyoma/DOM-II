e66e19e827724b9889eb2b3698f26b68ecf73043
Leynier Aragon <aragonleynier@gmail.com>
2020-06-08 22:57:20 -0400

    half way

--- js/index.js
+++ js/index.js
@@ -1,1 +1,28 @@
 // Your code goes here
+// NAV AREA
+
+const logoSpot = document.querySelector("h1");
+const bus = document.querySelector("img");
+
+//allows the logo area to be
+
+// alert box
+logoSpot.addEventListener("click", () => alert("Honk honk!"));
+
+// mouse enter/leave
+logoSpot.addEventListener("mouseenter", (e) => {
+  e.target.style.color = "purple";
+});
+logoSpot.addEventListener("mouseleave", (e) => {
+  e.target.style.color = "black";
+});
+
+//mid-section images
+
+const imgContent = document.querySelector("div.img-content");
+const mapImg = imgContent.querySelector("img");
+const boatImg = document.querySelector(".img-fluid rounded");
+
+mapImg.addEventListener("dblclick", (e) => {
+  e.src = "img/drive.jpg";
+});
